309514b7afe5469f1432645ece1b95235f517c1a
Ustin Vaskin <ustin.vaskin2011@gmail.com>
2020-08-08 21:44:42 +0500

    NV

---
+++ common/js/page_init.js
@@ -0,0 +1,27 @@
+/*
+ * page_init.js
+ * ---
+ * @author ustin.vaskin
+ * ---
+ * ---------------------------------------------
+ * (c) '
+ * ---------------------------------------------
+ */
+(function () {
+  $('.sections > .section:not(.hide):not(.show)').addClass('hide').waypoint({
+    handler: function (direction) {
+      $(this.element).removeClass('hide').addClass('show');
+      this.destroy();
+    },
+    offset: '80%'
+  });
+
+  $(function () {
+    Waypoint.refreshAll();
+    setTimeout(function () {
+      Waypoint.refreshAll();
+    }, 800)
+  })
+
+
+})(jQuery);
